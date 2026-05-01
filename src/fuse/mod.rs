fd7fc5205b03ac03550055ca678e584ccb5c734f
Esnglr <esinguler.eglr@proton.me>
2026-05-01 13:25:12 +0300

    Project skeloton initialized

---
+++ src/fuse/mod.rs
@@ -0,0 +1,1 @@
+// fuse klasorune handlers.rs veya file_ops.rs gibi dosyalar yaratilinca buraya da pub mod handlers; yazilacak
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod fuse;
+mod crypto;
+mod network;
+mod sandbox;
+
+fn main() {
+    println!("Zero-Trust Vault is starting...");
+    // gelecekte buraya cli (clap) kodlari gelecek
+}
